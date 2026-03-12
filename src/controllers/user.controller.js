97f5fae4f73fad5c242f4d84eba639916152dd12
Himanshu Mourya <119477557+Himanshu8728@users.noreply.github.com>
2026-03-12 12:36:08 +0530

    Fixed all Register Controllers Bugs

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -6,7 +6,7 @@ import { ApiResponse } from "../utils/ApiResponse.js";
 
 const registerUser = asyncHandler(async (req, res)=>{
     const {fullName, email, username, password} = req.body
-    console.log("email:", email)
+    //console.log("email:", email)
 
     if (
         [fullName, email, username, password].some((field)=>
@@ -25,7 +25,16 @@ const registerUser = asyncHandler(async (req, res)=>{
     console.log(req.files)
 
     const avatarLocalPath = req.files?.avatar[0]?.path;
-    const coverImageLocalPath = req.files?.coverImage[0]?.path;
+    //const coverImageLocalPath = req.files?.coverImage[0]?.path;
+
+    let coverImageLocalPath;
+    if(req.files && Array.isArray(req.files.coverImage) 
+    && req.files.coverImage.length > 0){
+        coverImageLocalPath = req.files.coverImage[0].path
+}
+    
+
+
 
     if(!avatarLocalPath){
         throw new ApiError(400, "Avatar file is required")
